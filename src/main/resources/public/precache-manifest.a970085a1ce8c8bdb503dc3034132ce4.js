096e3116007aba9480673e61444eefb2c42da990
AndriusKli <andriuskliunka@gmail.com>
2020-07-03 00:38:35 +0300

    updated version

--- src/main/resources/public/precache-manifest.a970085a1ce8c8bdb503dc3034132ce4.js
+++ src/main/resources/public/precache-manifest.a970085a1ce8c8bdb503dc3034132ce4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1ccb9dc67c17e8157497311347edbde8",
+    "revision": "19a762cdd10d5955cd3121c21bd057b4",
     "url": "/index.html"
   },
   {
-    "revision": "cf4060b5e45f921ada12",
+    "revision": "39db244de30d991db983",
     "url": "/static/css/main.8e75e2e4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.9bb4acea.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cf4060b5e45f921ada12",
-    "url": "/static/js/main.c1e6342e.chunk.js"
+    "revision": "39db244de30d991db983",
+    "url": "/static/js/main.c87e1c96.chunk.js"
   },
   {
     "revision": "3db2f682136419107866",
--- src/main/resources/public/service-worker.js
+++ src/main/resources/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a37466ef4564ebaa228c52ab02133c74.js"
+  "/precache-manifest.a970085a1ce8c8bdb503dc3034132ce4.js"
 );
 
 self.addEventListener('message', (event) => {
